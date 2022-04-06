115798286ce961c4096d6adf1dbd35ef3c669b29
Hyu-Phan <pqhuy27122000@gmail.com>
2022-04-06 10:09:42 +0700

    "pom parent"

--- management-library-api/src/main/java/com/elcom/library/LibraryApplication.java
+++ management-library-api/src/main/java/com/elcom/library/LibraryApplication.java
@@ -2,8 +2,10 @@ package com.elcom.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.elcom")
 public class LibraryApplication {
 
 	public static void main(String[] args) {
